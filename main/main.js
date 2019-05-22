f84bdb1e82d19989577e53ffd776f1fbaf81daab
yuanqingzhu <yuanqingzhu@tencent.com>
2019-05-22 16:55:04 +0800

    add main.js to pass unittest

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var distance_fee, wait_fee, total_fee;
+
+    if (inputs.distance <= 2)
+        distance_fee = 6
+    else if (inputs.distance > 2 && inputs.distance <= 8)
+        distance_fee = 6 + 0.8 * (inputs.distance - 2)
+    else
+        distance_fee = 6 + 0.8 * (8 - 2) + 0.8 * (1 + 0.5) * (inputs.distance - 8)
+    
+    wait_fee = inputs.parkTime * 0.25
+    total_fee = Math.round(distance_fee + wait_fee)
+    return total_fee;
 };
